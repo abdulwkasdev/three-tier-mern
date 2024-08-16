83cb4232cfcda25c3573084cf4191d76aebb6235
Wahab Kasumu <wahabkas@gmail.com>
2024-08-15 22:28:39 -0400

    created docker compose

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,12 @@ dotenv.config()
 
 export const PORT = process.env.PORT;
 
-export const mongoDBURL = process.env.MONOGODB_URI
+export const mongoDBURL = `mongodb://${process.env.MONOGODB_USERNAME}:${process.env.MONOGODB_PASSWORD}@${process.env.MONOGODB_URI}:27017`
+// export const mongoDBURL = process.env.MONOGODB_URI
+// "mongodb://${process.env.MONOGODB_USERNAME}:${process.env.MONOGODB_PASSWORD}@${process.env.MONOGODB_URI}:27017"
+//export const mongoDBUSERNAME = process.env.MONOGODB_USERNAME
+//export const mongoDBPASSWORD = process.env.MONOGODB_PASSWORD
+
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
